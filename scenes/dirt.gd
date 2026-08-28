3e70c862bf0151e81bfaf9ab7e7cdf808a6b586f
Jonas Rettig <jrettig@uni-muenster.de>
2026-08-29 01:45:15 +0200

    Sheep Dirt besser

--- scenes/dirt.gd
+++ scenes/dirt.gd
@@ -1,1 +1,19 @@
 extends Area2D
+
+var is_getting_cleaned = false
+var dirt_alpha = 1
+
+func _mouse_enter() -> void:
+	is_getting_cleaned = true
+	
+func _mouse_exit() -> void:
+	is_getting_cleaned = false
+
+func _process(_delta: float) -> void:
+	if(is_getting_cleaned):
+		print("dirt is getting cleaned")
+		self.modulate = Color(self.modulate, dirt_alpha)
+		dirt_alpha -= 0.008
+	
+	if(dirt_alpha < 0.1):
+		self.visible = false
--- scenes/sheep.gd
+++ scenes/sheep.gd
@@ -1,23 +1,27 @@
 extends Sprite2D
 
-var is_in_hover = false 
+var is_in_hover_on_body = false 
+var is_hover_on_dirt = false 
+
 var time_cared_for_sheep = 0.0
 var sheep_care = 0.0
 
-@onready var dirt = $Dirt
-
 func _on_dirt_mouse_entered() -> void:
-	sheep_care += 10
-	dirt.visible = false
+	is_hover_on_dirt = true
+
+func _on_dirt_mouse_exited() -> void:
+	is_hover_on_dirt = false
 
 func _on_sheep_body_mouse_entered() -> void:
-	is_in_hover = true
-
+	is_in_hover_on_body = true
 
 func _on_sheep_body_mouse_exited() -> void:
-	is_in_hover = false
+	is_in_hover_on_body = false
 
 func _process(delta: float) -> void:
-	if(is_in_hover):
+	if(is_in_hover_on_body):
 		time_cared_for_sheep += delta
 		print(time_cared_for_sheep)
+		
+	if(is_hover_on_dirt):
+		sheep_care += 0.1
