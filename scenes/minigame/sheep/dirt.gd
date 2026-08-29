942f7cd561a7a5ce4d43dfa3efb69fcc7006e62e
Jonas Rettig <jrettig@uni-muenster.de>
2026-08-29 19:32:27 +0200

    Dirt dirtier

--- scenes/minigame/sheep/dirt.gd
+++ scenes/minigame/sheep/dirt.gd
@@ -4,6 +4,31 @@ var is_hovering = false
 var dirt_alpha = 1
 var is_scored = false
 
+@export var which_dirt = "head"
+
+@onready var dirt_head = $DirtHead
+@onready var dirt_up_right = $DirtUpRight
+@onready var dirt_top_mid = $DirtTopMid
+@onready var dirt_down_left = $DirtDownLeft
+@onready var dirt_down_right = $DirtDownRight
+
+func _ready() -> void:
+	if(which_dirt == "head"):
+		dirt_head.visible = true
+		dirt_head.disabled = false
+	elif(which_dirt == "up_right"):
+		dirt_up_right.visible = true
+		dirt_up_right.disabled = false
+	elif(which_dirt == "top_mid"):
+		dirt_top_mid.visible = true
+		dirt_top_mid.disabled = false
+	elif(which_dirt == "down_left"):
+		dirt_down_left.visible = true
+		dirt_down_left.disabled = false
+	elif(which_dirt == "down_right"):
+		dirt_down_right.visible = true
+		dirt_down_right.disabled = false
+
 func _mouse_enter() -> void:
 	is_hovering = true
 	
