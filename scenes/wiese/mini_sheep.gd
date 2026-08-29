e3339275e4bef851595c0767cca6574f969dbf8d
Corneey <konstantinbock@yahoo.de>
2026-08-30 00:45:19 +0200

    Better wiesen-movement

--- scenes/wiese/mini_sheep.gd
+++ scenes/wiese/mini_sheep.gd
@@ -4,8 +4,7 @@ signal open_sheep
 
 @export var speed = 100
 
-@export var max_y = 600
-@export var max_x = 600
+var roaming_area: Rect2
 
 var target
 var rng = RandomNumberGenerator.new()
@@ -13,6 +12,7 @@ var is_hovered = false
 var already_cared_for = false
 
 func _ready() -> void:
+	global_position = get_random_location()
 	target = get_random_location()
 	
 func _process(_delta: float) -> void:
@@ -27,7 +27,7 @@ func _open_sheep() -> void:
 		already_cared_for = true
 		self.modulate = Color(self.modulate, 0.5)
 
-func _physics_process(_delta):
+func _physics_process(_delta):	
 	self.velocity = global_position.direction_to(target) * speed
 	if global_position.distance_to(target) > 10:
 		move_and_slide()
@@ -35,7 +35,10 @@ func _physics_process(_delta):
 		target = get_random_location()
 
 func get_random_location():
-	return Vector2(rng.randf_range(0, max_x),rng.randf_range(0, max_y))
+	return Vector2(
+		rng.randf_range(roaming_area.position.x, roaming_area.end.x),
+		rng.randf_range(roaming_area.position.y, roaming_area.end.y)
+		)
 
 func _on_area_2d_mouse_entered() -> void:
 	is_hovered = true
