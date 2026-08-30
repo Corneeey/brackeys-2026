c6ca594da5fe86c7d6facd3dbcf0382675b209e2
Corneey <konstantinbock@yahoo.de>
2026-08-30 12:37:00 +0200

    Disable story

--- scenes/dating_sim/story_beat/story_beat.gd
+++ scenes/dating_sim/story_beat/story_beat.gd
@@ -3,6 +3,7 @@ class_name StoryBeat extends TextureRect
 signal story_beat_finished(story_beat)
 signal change_music(music)
 
+var disabled = false
 var visuals: Texture
 var music: AudioStream
 
@@ -15,5 +16,9 @@ func _ready() -> void:
 
 
 func _on_gui_input(_event: InputEvent) -> void:
+	if disabled:
+		return
+	
 	if (Input.is_action_just_pressed("ui_click")):
+		disabled = true
 		story_beat_finished.emit(self)
